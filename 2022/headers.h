3686bc357eea66ff092c750954b2f2900cf30696
Anass Taher <AnassTaher2003@hotmail.com>
2023-11-26 13:11:20 +0100

    2022-Day2,3

---
+++ 2022/headers.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <stdlib.h>
+
+
+using namespace std;

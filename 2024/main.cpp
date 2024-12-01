6040833f2e76a709e4ffd2592d8bbb7b98d64495
Anafo <AnassTaher2003@hotmail.com>
2024-12-01 13:29:18 +0100

    Updating framework, removing endl

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <cstdio>
 
-int get_ex1(std::vector<std::string>& input, bool debug);
-int get_ex2(std::vector<std::string>& input, bool debug);
+int get_ex1(std::vector<std::string>& input, bool exercise_nbr);
+int get_ex2(std::vector<std::string>& input, bool exercise_nbr);
 
 std::vector<std::string> get_input(int day, std::string& file_type_name)
 {
@@ -26,23 +26,23 @@ int main(int argc, char* argv[])
 
   if (argc < 2)
   {
-    std::cerr << "Call program as follows: ./out <day_number>" << std::endl;
+    std::cerr << "Call program as follows: ./out <day_number>\n";
     return EXIT_FAILURE;
   }
   
-  bool debug = false;
+  int exercise_nbr = 0;
   int day = 0;
 
   if (argc >= 2)
   {
     day = atoi(argv[1]);
-    std::cout << "Running program for Day " + std::to_string(day) << std::endl;
+    std::cout << "Running program for Day " + std::to_string(day) << "\n";
   }
 
   if (argc >= 3)
   {
-    debug = atoi(argv[2]);
-    std::cout << "Running program in debug mode" << std::endl;
+    exercise_nbr = atoi(argv[2]);
+    std::cout << "Running exercise: " << exercise_nbr << "\n";
   }
 
   std::cout << "Which input would you like to choose?\n"
@@ -70,26 +70,26 @@ int main(int argc, char* argv[])
   std::vector<std::string> input = get_input(day, file_type_name);
 
   std::cout << "Ex1\n";
-  int res_1 = get_ex1(input, debug);
+  int res_1 = get_ex1(input, exercise_nbr);
   std::cout << "Result Ex1 = " << res_1 << "\n";
 
   std::cout << "Ex2\n";
-  int res_2 = get_ex2(input, debug);
+  int res_2 = get_ex2(input, exercise_nbr);
   std::cout << "Result Ex2 = " << res_1 << "\n";
 
   if (!res_1)
   {
-    std::cerr << "Error: Ex1 failed" << std::endl;
+    std::cerr << "Error: Ex1 failed\n";
     return EXIT_FAILURE;
   }
 
   if (!res_2)
   {
-    std::cerr << "Error: Ex2 Failed" << std::endl;
+    std::cerr << "Error: Ex2 Failed\n";
     return EXIT_FAILURE;
   }
  
-  std::cout << "GG" << std::endl;
+  std::cout << "GG\n";
   return EXIT_SUCCESS;
 }
 
